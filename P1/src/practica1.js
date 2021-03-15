cbf1717d4637d3673bcdb8332d913d018f3ae42e
albertomog88 <80414409+albertomog88@users.noreply.github.com>
2021-03-15 21:31:08 +0100

    Add files via upload

--- P1/src/practica1.js
+++ P1/src/practica1.js
@@ -1,21 +1,17 @@
 "use strict";
 
-console.log( "Practica 1");
 
-//var MemoryGame = MemoryGame;
 
 var MemoryGame = function(gs) {
 
 	this.gs = gs;
-	//this.caratulas = new Array(16);
 	this.numEncontradas = 0;//8parejas
 	this.mensajeEstado = "";
 	
 	//: Inicializa el juego creando las cartas (recuerda que son 2 de cada
 	//tipo de carta), desordenándolas y comenzando el bucle de juego.
 	this.initGame = function(){
-		console.log("Iniciando Juego");
-
+		
 		this.caratulas = [
 			new MemoryGameCard("perico"),
 			new MemoryGameCard("mortadelo"),
@@ -38,7 +34,7 @@ var MemoryGame = function(gs) {
 		];
 
 		//Desordenar baraja (PARA PRUEBAS COMENTAMOS)
-		//this.caratulas.sort(function() { return Math.random() - 0.5 });
+		this.caratulas.sort(function() { return Math.random() - 0.5 });
 		
 		//Cargar sprite inicial.
 		this.mensajeEstado = "Bienvenido a Memory Card";
@@ -46,23 +42,11 @@ var MemoryGame = function(gs) {
 
 	}
 
-	// 	Dibuja el juego, esto es: (1) escribe el mensaje con el estado actual del
-	// juego y (2) pide a cada una de las cartas del tablero que se dibujen.
-	this.draw = function(){
-		console.log("Pintando");
-		
-		gs.drawMessage(this.mensajeEstado);
-		var i = 0;
-		for(i ;i < this.caratulas.length ; i++)
-			this.caratulas[i].draw(this.gs, i);
-	}
-
 	// 	Es el bucle del juego. En este caso es muy sencillo: llamamos al método
 	// draw cada 16ms (equivalente a unos 60fps). Esto se realizará con la función
 	// setInterval de Javascript.
 	
 	this.loop = function(){
-		console.log("bucle");
 		var that = this;
 		//https://developer.mozilla.org/es/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
 		setInterval(function(){	
@@ -70,59 +54,56 @@ var MemoryGame = function(gs) {
 		}, 
 		16); //16 Milisegundos de refresco
 	}
-	this.reinicio;
+
+
+	// 	Dibuja el juego, esto es: (1) escribe el mensaje con el estado actual del
+	// juego y (2) pide a cada una de las cartas del tablero que se dibujen.
+	this.draw = function(){
+		
+		gs.drawMessage(this.mensajeEstado);
+		for(var i = 0;i < this.caratulas.length ; i++)
+			this.caratulas[i].draw(this.gs, i);
+	}
+
 	this.pareja = undefined;//Nombre de la carta
 	this.idCardPar = undefined;
 
 	this.onClick = function(cardId){
-		console.log("onclick");
-		//this.mensajeEstado = this.caratulas[cardId].nombre;
 		this.statusCard_Game(cardId);//COMPROBACION
 		if(cardId != undefined //Si no es undefined
 			&& this.caratulas[cardId].status().std!=0 //Si no ha sido encontrada
 			&& this.caratulas[cardId].status().std!=1){ // Si no esta bocarriba
 
 				this.caratulas[cardId].flip();//Volteamos carta
-				
+				this.mensajeBox("b", undefined, undefined);
 				if(this.pareja!= undefined){//Si hay alguna carta sin pareja comprobamos
-					console.log('Comprobamos si la prajera es correcta!');
 					if(!this.caratulas[cardId].compareTo(this.pareja) ){
-						
-						console.log('No hay pareja');
-						//PROBLEMAS PARA PINTAR MENSAJE!!!!!!
-						//this.textoEstado = this.caratulas[cardId].nombre +" y "+ this.pareja+" no son pareja.";
+						this.mensajeBox("np", this.pareja, this.caratulas[cardId].nombre);
 						var that = this;
 						setTimeout( function(){
-							console.log('DENTRO '+cardId);
 							that.caratulas[cardId].flip();
 							that.caratulas[that.idCardPar].flip();//PROBLEMAS POR QUE LE PASO UN ID
 							that.cartaVolteada = undefined;
 							that.pareja = undefined;
 							that.idCardPar = undefined;
-							
-						}, 1000);
-						
-						// console.log('Carta encontrada!');
-						
+							}, 
+						650);
 					}
 					else{ //No hay pareja, damos la vuelta y des
-						console.log('Carta  encontrada!');
-						this.parejaEncontrada(cardId, this.idCardPar);
-						
-						
+						//Marcamos las cartas como encontradas
+						this.caratulas[cardId].found();
+						this.caratulas[this.idCardPar].found();
+						//Aumentamos contador de parejas
 						this.numEncontradas++;//MAX 8 PAREJAS
-						this.mensajeEstado = this.pareja + " pareja encontrada!";
+						//this.mensajeEstado = this.pareja + " pareja encontrada!";
+						this.mensajeBox("p", this.pareja, undefined);
 						this.pareja = undefined; //Desmarcamos
 						
 						if(this.numEncontradas == 8){
-							this.numEncontradas = "Ganaste!";
-							//FIN DE LA PARTIDA IDEAS
-							//SISTEMA DE PUNTUACIONES, ?? COMPLICARSE
-							//OPCION A REINICIAR JUEGO?
-							console.log('GANASTE!');
-							this.mensajeEstado ="GANASTE!";
-							var nuevo = confirm("¿Desea juegar de nuevo?");
-							nuevo ? location.reload():this.mensajeEstado="Fin del juego";
+							//this.mensajeEstado ="GANASTE!";
+							this.mensajeBox("w", undefined, undefined);
+							// var nuevo = confirm("¿Desea jugar de nuevo?");
+							// nuevo ? location.reload():this.mensajeEstado="Fin del juego";
 						}
 					}
 				}
@@ -133,15 +114,51 @@ var MemoryGame = function(gs) {
 		}
 	}
 
-	this.parejaEncontrada = function(cardID1, cardID2){
-		this.caratulas[cardID1].found();
-		this.caratulas[cardID2].found();
-		//this.mensajeEstado = this.caratulas[cardID1].nombre + " pareja encontrada!";
-		//DAR MENSAJE DE EL NOMBRE DE LA PAREJA ENCONNTRADO O SIMILAR
-	}
-
 	
 
+	
+	this.mensajeBox = function(opt, cardWin, cardPar){
+		console.log("booox");
+		if(opt=="w"){
+			let msnWin = [
+				"“Si puedes aceptar perder, puedes ganar”",
+				"“Nunca pierdo, o gano o aprendo”",
+				"“A veces ganas y a veces aprendes”",
+				"“La diferencia entre ganar y perder es no renunciar”"
+			];
+			msnWin.sort(function() { return Math.random() - 0.5 });
+			this.mensajeEstado = "WIN! "+msnWin[0];
+		}
+		else if(opt=="p" && cardWin!=undefined){
+			let pareja = [
+				"Carta "+ cardWin + " ha encontrado a su media naranja", 
+				"Carta "+ cardWin + " con pareja!",
+				"Pareja encontrada!"
+			];
+			pareja.sort(function() { return Math.random() - 0.5 });
+			this.mensajeEstado = pareja[0];
+		}
+		else if(opt=="np"){
+			let no_pareja = [
+				cardWin + " y "+ cardPar+ " no son pareja",
+				"Sigue buscando",
+				cardWin + " y "+ cardPar+ " no son iguales"
+			];
+			no_pareja.sort(function() { return Math.random() - 0.5 });
+			this.mensajeEstado = no_pareja[0];
+		}
+		else if(opt=="b"){
+			let buscando = [
+				"Buscando pareja",
+				"Sondeando cartas",
+				"Continua buscando", 
+				"¿Donde esta esa carta?",
+				"No te escondas"
+			];
+			buscando.sort(function() { return Math.random() - 0.5 });
+			this.mensajeEstado = buscando[0];
+		}
+	}
 	
 
 	this.statusCard_Game = function(cardId){
